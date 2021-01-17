37d66fd2437216864555303ede312ab8d22c115f
Mario <marioballestero.rdg@gmail.com>
2021-01-17 22:59:34 +0100

    fix some bugs

--- config/database.js
+++ config/database.js
@@ -4,8 +4,13 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: "postgres",
+        host: env("DATABASE_HOST", "127.0.0.1"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "procrest"),
+        username: env("DATABASE_USERNAME", "procrest"),
+        password: env("DATABASE_PASSWORD", "procrest123!"),
+        ssl: env.bool("DATABASE_SSL", false),
       },
       options: {
         useNullAsDefault: true,
