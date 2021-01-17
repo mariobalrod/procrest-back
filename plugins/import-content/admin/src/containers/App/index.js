4ecf55d6a3e1e76052d347c4937e726382f5f9ff
Mario <marioballestero.rdg@gmail.com>
2021-01-17 23:10:15 +0100

    feat: add plugin

--- plugins/import-content/admin/src/containers/App/index.js
+++ plugins/import-content/admin/src/containers/App/index.js
@@ -5,19 +5,25 @@
  *
  */
 
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import { NotFound } from 'strapi-helper-plugin';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import { NotFound } from "strapi-helper-plugin";
 // Utils
-import pluginId from '../../pluginId';
+import pluginId from "../../pluginId";
 // Containers
-import HomePage from '../HomePage';
+import HomePage from "../HomePage";
+import HistoryPage from "../HistoryPage";
 
 const App = () => {
   return (
     <div>
       <Switch>
         <Route path={`/plugins/${pluginId}`} component={HomePage} exact />
+        <Route
+          path={`/plugins/${pluginId}/history`}
+          component={HistoryPage}
+          exact
+        />
         <Route component={NotFound} />
       </Switch>
     </div>
